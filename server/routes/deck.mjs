1b3a4780f752f4184a21cd15a5a70ed6619732da
Echo Echo Echo <39967300+Shhwip@users.noreply.github.com>
2023-07-12 19:38:06 -0600

    significant work on the decks

--- server/routes/deck.mjs
+++ server/routes/deck.mjs
@@ -1,17 +1,59 @@
+import express from 'express';
+import db from "../config/db.mjs"
+import CARDS from "../resources/cardslist.mjs"
+
+const router = express.Router();
+
+
+function shuffle(array) {
+    let currentIndex = array.length
+    let randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
 
 // ----------------------------------------------
 // Make new shuffled deck of cards
-// parameters: none
+// url: /deck/new
+// request body: none
 // returns the following object:
-// {
-//   "success": true,
-//   "deck_id": "3p40paa87x90",
-//   "remaining": 52
-// }
+//{
+//    "acknowledged": true,
+//    "insertedId": "64af4ef4a688f1539bebb87d"
+//}
+router.get('/deck/new', async (req, res) => {
+    let decklist = new Array(52);
+    for(let i = 0; i < 52; i++)
+    {
+        decklist[i] = i + 1;
+    }
+    shuffle(decklist);
+    let deck = {
+        list : decklist,
+        index : 0
+    }
+    let collection = await db.collection("decks");
+    let result = await collection.insertOne(deck);
+    res.send(result).status(204);
+});
+
+
 
 // ----------------------------------------------
 // Draw cards from deck or pile
-// parameters: deck_id, count
+// url: /{pile_id}/{number_of_cards_to_draw}
+// request body: none
 // Draw(3p40paa87x90, 2)
 // returns the following object:
 // {
@@ -28,13 +70,33 @@
 //       "reference": "3_of_diamonds",
 //     }
 //   ],
-//   "deck_id": "3p40paa87x90",
+//   "pile_id": "3p40paa87x90",
 //   "remaining": 50
 // }
+router.patch('/draw/:pile_id/:number', async (req, res) => {
+    let query = {_id: new ObjectId(req.params.pile_id)};
+    let number = req.params.number;
+
+    let collection = await db.collection("decks");
+    let deck = await collection.findOne(query);
+    if(!deck)
+    {
+        res.send("pile not found").status(404);
+    }
+    let cards_list = new Array();
+    for(let i = deck.index; i < number + deck.index; i++)
+    {
+        cards_list.push(deck.list[i]);
+    }
+
+    res.send(cards_list).status(200);
+});
+
 
 // ----------------------------------------------
 // Create pile
-// parameters: deck_id/pile_id, pile_name, number_of_cards
+// url: /new_pile/{id of original pile}/{new pile name}/{number of cards}
+// request body: none
 // CreatePile(3p40paa87x90, discard_1, 20)
 // returns the following object:
 // {
@@ -43,21 +105,31 @@
 //   "pile_id": "3p40paa87x90_discard_1",
 //   "remaining": 20
 // }
+router.get('/new_pile/:pile_id/:new_pile_name/:number', async (req, res) => {
+
+
+});
 
 // ----------------------------------------------
 // Add cards to pile
-// parameters: pile_id, array of cards
-// AddToPile(3p40paa87x90_discard_1, [2, 3])
+// url: /add/{pile id}
+// request body: array of cards
+// AddToPile(3p40paa87x90_discard_1, 2)
 // returns the following object:
 // {
 //   "success": true,
 //   "pile_id": "3p40paa87x90_discard_1",
 //   "remaining": 22
 // }
+router.post('/add/:pile_id', async (req, res) => {
+
+
+});
 
 // ----------------------------------------------
 // Combine and shuffle piles
-// parameters: array of piles, pile_name
+// url: /combine/{final pile id}
+// request body: array of pile id's
 // CombinePiles([3p40paa87x90_discard_1, 3p40paa87x90_discard_2], discard_3)
 // returns the following object:
 // {
@@ -65,3 +137,9 @@
 //   "remaining": 42
 //   "pile_id": "3p40paa87x90_discard_3",
 // }
+router.post('/combine/:pile_id', async (req, res) => {
+
+
+});
+
+export default router;
--- server/server.mjs
+++ server/server.mjs
@@ -1,41 +1,44 @@
 import express from 'express';
 import cors from "cors";
 import db from "./config/db.mjs"
+import deck from "./routes/deck.mjs"
 
 const app = express();
 
-const port = 5050;
+const port = 5055;
 
 const router = express.Router();
 
 
 app.use(cors());
+app.use("/", deck);
 app.use(express.json());
 
 // Test the first collection
 async function testCollection() {
-  try {
-    const myFirstCollection = db.collection('MyfirstCollection');
-    await myFirstCollection.insertOne({ groupName: 'Outoftown' });
-    console.log('Inserted document into MyfirstCollection');
-  } catch (error) {
-    console.error('Error inserting document:', error);
-  }
+  // try {
+  //   const myFirstCollection = db.collection('MyfirstCollection');
+  //   await myFirstCollection.insertOne({ groupName: 'Outoftown' });
+  //   console.log('Inserted document into MyfirstCollection');
+  // } catch (error) {
+  //   console.error('Error inserting document:', error);
+  // }
 }
+
 
 testCollection();
 
-// Handle GET requests
-router.get('/', async (req, res) => {
-  try {
-    const collection = db.collection('MyfirstCollection');
-    const results = await collection.find({}).toArray();
-    res.send(results).status(200);
-  } catch (error) {
-    console.error('Error retrieving data:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+// // Handle GET requests
+// router.get('/', async (req, res) => {
+//   try {
+//     const collection = db.collection('MyfirstCollection');
+//     const results = await collection.find({}).toArray();
+//     res.send(results).status(200);
+//   } catch (error) {
+//     console.error('Error retrieving data:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
 
 // Assign the router to the root URL
 app.use('/', router);
