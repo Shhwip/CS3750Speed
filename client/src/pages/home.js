0445ef34f759849d9e82b5e9c0baded5e587b3e4
Echo Echo Echo <39967300+Shhwip@users.noreply.github.com>
2023-07-14 15:34:34 -0600

    added links to the homepage for easier testing

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const HomePage = () => {
-  return <h1>This is home page</h1>;
+  return <div>
+    <h1>This is home page</h1>
+    <h2>Click on the links below to navigate</h2>
+    <div>
+    <a href="/register"> register </a>
+    </div>
+    <div>
+    <a href="/login"> login </a>
+    </div>
+    <div>
+    <a href="/lobby">lobby</a>
+    </div>
+    </div>;
 };
 
 export default HomePage;
