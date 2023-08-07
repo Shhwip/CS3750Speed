ada3af966e24ec30092473eab623755ef36cc50b
Echo Echo Echo <39967300+Shhwip@users.noreply.github.com>
2023-08-07 00:13:09 -0600

    added newgame to classicSpeed

--- server/routes/games/classicSpeed.mjs
+++ server/routes/games/classicSpeed.mjs
@@ -5,15 +5,66 @@ import { ObjectId } from "mongodb";
 
 const classicSpeed = express.Router();
 
-classicSpeed.get('/new', async (req, res) => {
+classicSpeed.post('/new', async (req, res) => {
+    let userName = req.body.userName;
+    let gameID = req.body.gameID;
     let gameState = await newGame();
+
+    let player = async (userName, gameID) => {
+        let collection = db.collection("Game-Room");
+        let query = {_id: new ObjectId(gameID)};
+        let gameRoom = await collection.findOne(query);
+        console.log(gameRoom);
+        console.log(gameRoom.user1 === userName);
+        if(gameRoom.user1 === userName) {
+            return "player1";
+        } else if(gameRoom.user2 === userName) {
+            return "player2";
+        }else {
+            return "spectator";
+        }
+    }
+    player = await player(userName, gameID);
+    console.log(player);
+    if(player === "player1") {
+        gameState.gameState.hand2 = [];
+        console.log(gameState.hand2);
+        gameState.gameState.deck2 = [];
+    } else if(player === "player2") {
+        gameState.gameState.hand1 = [];
+        gameState.gameState.deck1 = [];
+    }
+    console.log(gameState);
     res.send({gameState}).status(200);
 });
 
-classicSpeed.get('/:gameID', async (req, res) => {
-    console.log(req.params.gameID);
+classicSpeed.get('/:gameID/:userName', async (req, res) => {
+    console.log(req.params);
+    gameID = req.params.gameID;
+    userName = req.params.userName;
     let result = await gameState(req.params.gameID);
-    
+
+    let player = async (userName, gameID) => {
+        let collection = db.collection("Game-Room");
+        let query = {_id: new ObjectId(gameID)};
+        let gameRoom = await collection.findOne(query);
+        if(gameRoom.user1 === userName) {
+            return "player1";
+        } else if(gameRoom.user2 === userName) {
+            return "player2";
+        }else {
+            return "spectator";
+        }
+    }
+
+    if(player === "player1") {
+        result.gameState.hand2 = [];
+        result.gameState.deck2 = [];
+    } else if(player === "player2") {
+        result.gameState.hand1 = [];
+        result.gameState.deck1 = [];
+    }
+
     res.send(result).status(200);
 });
 
@@ -29,7 +80,7 @@ async function gameState(gameID) {
 
 // ----------------------------------------------
 // New Game
-async function newGame() {
+async function newGame(player) {
     let deckID = await createDeck();
     console.log(deckID);
     let deck = await draw(deckID, 52);
@@ -42,7 +93,7 @@ async function newGame() {
         centerDeckRight: deck.slice(45, 50),
         playPileLeft: deck.slice(50, 51),
         playPileRight: deck.slice(51, 52),
-}
+    }
 
     let collection = db.collection("classicSpeedGames");
     let gameID = await collection.insertOne(gameState);
