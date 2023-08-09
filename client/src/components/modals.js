39a6ff00727c77ec1574f1f8096bc986a426140e
Jose Cordova <JoseRincon@mail.weber.edu>
2023-08-09 17:43:45 -0600

    added modal for waiting room

---
+++ client/src/components/modals.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { ModalBody } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Spinner from 'react-bootstrap/Spinner'
+
+export default function WaitingModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Waiting for other player....
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Footer>
+        <Button onClick={props.handleClose} >
+          Close
+        </Button>
+        <Button disabled="false">
+          Waiting
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          />
+        </Button>          
+      </Modal.Footer>
+    </Modal>
+  );
+}
