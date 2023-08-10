4706d4a0b7b074932cb4bdaeab45cbb7fa665d0d
Qui Ngo <quingo@mail.weber.edu>
2023-08-10 17:40:00 -0600

    add play again functionality

--- client/src/pages/waitingRoom.js
+++ client/src/pages/waitingRoom.js
@@ -4,21 +4,15 @@ import socket from "../socket";
 import Timer from "../components/timer";
 import Classic from "./classic";
 import { useNavigate } from "react-router-dom";
-import WaitingModal from "../components/modals";
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import Avatar from '../png/LinkedIn-Silhouette (1).jpg'
 const WaitingRoomPage = () => {
   const location = useLocation();
   const numPlayer = location.state.numPlayer;
-
+  
   const { id } = useParams();
   const [room, setRoom] = useState({});
   const [numPlayerReady, setPlayerReady] = useState(0);
   const [startGame, setStartGame] = useState(false);
   const [showClassic, setShowClassic] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [closeClicked, setCloseClicked] = useState(false);
   const navigate = useNavigate();
 
   const fetchRoom = async () => {
@@ -43,8 +37,8 @@ const WaitingRoomPage = () => {
     joinRoom();
     fetchRoom();
 
-    const handleUserJoined = () => {
-      console.log('Another user joined the room');
+    const handleUserJoined = (data) => {
+      console.log('Another user joined the room: ');
       fetchRoom();
     };
 
@@ -58,11 +52,9 @@ const WaitingRoomPage = () => {
   useEffect(() => {
 
     const handleReceiveMessage = (data) => {
-      console.log(numPlayerReady)
       setPlayerReady(data.numPlayerReady);
       if (data.numPlayerReady === 2) {
         setStartGame(true);
-        setShowModal(false);
       }
     };
 
@@ -83,37 +75,34 @@ const WaitingRoomPage = () => {
 
   const handleReadyPlayer = async () => {
     const updatedNumPlayerReady = numPlayerReady + 1;
-
-    setPlayerReady(updatedNumPlayerReady)
+    setPlayerReady(updatedNumPlayerReady);
     socket.emit("join_room", { roomId: room._id, numPlayerReady: updatedNumPlayerReady });
-    setShowModal(true);
-
   };
 
-  const leaveRoomClick = async () => {
-    const roomId = { id: id }
-    console.log(roomId);
+  const leaveRoomClick = async() => {
+      const roomId = {id: id }
+      console.log(roomId);
     try {
-      const response = await fetch(`http://localhost:5050/api/room/deleteRoom/${id}`, {
-        method: "DELETE",
-        body: JSON.stringify({ _id: roomId })
-      });
-      const data = await response.json();
-      if (!response.ok) {
-        throw new Error(data.message || "Failed to fetch room");
+        const response = await fetch(`http://localhost:5050/api/room/deleteRoom/${id}`, { 
+          method: "DELETE",
+          body: JSON.stringify({_id: roomId})
+        });
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message || "Failed to fetch room");
+        }
+        else {
+          // altered from navigate to socket.emit
+          socket.emit("leave_room", roomId);
+          
+        }
+      } catch (error) {
+        console.error(error);
       }
-      else {
-        // altered from navigate to socket.emit
-        socket.emit("leave_room", roomId);
-
-      }
-    } catch (error) {
-      console.error(error);
-    }
 
   }
-
-  useEffect(() => {
+  
+  useEffect(()=>{
     const leaveRoom = () => {
       navigate("/lobby");
     }
@@ -122,60 +111,23 @@ const WaitingRoomPage = () => {
     return () => {
       socket.off("leave_room", leaveRoom);
     }
-  })
-
-  if (numPlayerReady == 0) {
-    var t = "No"
-  }
-  else {
-    t = "YES"
-  }
-
+  })  
+  
   return (
     <>
       {showClassic ? (
-        <Classic numPlayer={numPlayer} room={room} />
+        <Classic numPlayer= {numPlayer} room = {room}  setShowClassic={setShowClassic} setPlayerReady={setPlayerReady} setStartGame={setStartGame}/>
       ) : (
         <>
-          
-          <h1 className="waitingRoom-title">{"WAITING ROOM"} </h1>
-          <h2>{"Game Type: " + room.gameType}</h2>
-          <CardGroup className="w-50">
-            <Card className="m-5">
-              <Card.Img  variant="top" src="https://images.squarespace-cdn.com/content/v1/50f79c6fe4b00d3480c9bbf0/1603139268457-L8WP2GF1EDTQS1YIEJLY/LinkedIn-Silhouette.jpg"/>
-              <Card.Body>
-                <Card.Title>USER:</Card.Title>
-                <Card.Text>
-                  {room.user1}
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <button onClick={handleReadyPlayer}>Ready</button>
-                <button onClick={leaveRoomClick}>Leave Room</button>
-              </Card.Footer>
-            </Card>
-            <Card className="m-5">
-              <Card.Img variant="top" src="https://images.squarespace-cdn.com/content/v1/50f79c6fe4b00d3480c9bbf0/1603139268457-L8WP2GF1EDTQS1YIEJLY/LinkedIn-Silhouette.jpg" />
-              <Card.Body>
-                <Card.Title>USER:</Card.Title>
-                <Card.Text>
-                  {room.user2}
-                </Card.Text>
-                <Card.Footer>
-                  Player Ready: {t}
-                </Card.Footer>
-              </Card.Body>
-            </Card>
-            <div className="m-5">
-              {startGame ? <Timer /> : null}
-            </div>
-          </CardGroup>
-          <div className="row">
-            <h3>Players Ready: {numPlayerReady} </h3>
-            <WaitingModal show={showModal} handleClose={() => setShowModal(false) || setCloseClicked(true) || setPlayerReady(numPlayerReady - 1)} />
-            
-          </div>
-          </>
+          {startGame ? <Timer /> : null}
+          <h1>{"This is waiting room, game type: " + room.gameType}</h1>
+          <h2>{room.user1}</h2>
+          <h2>{room.user2}</h2>
+          <button onClick={handleReadyPlayer}>Ready</button>
+		  <div>
+        <button onClick={leaveRoomClick}>Leave Room</button>
+      </div>
+        </>
       )}
     </>
   );
